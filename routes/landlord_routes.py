2add0351606fc1d0fc56139f20b4a8ce8270e041
kennedykirui766-beep <kennykirui766@gmail.com>
2026-04-03 00:48:36 +0300

    exempt csrf

--- routes/landlord_routes.py
+++ routes/landlord_routes.py
@@ -354,10 +354,10 @@ def messages():
 
 
 # ---------------- Profile ----------------
-from flask_wtf.csrf import exempt
+@csrf.exempt
 @landlord_bp.route("/profile", methods=["GET", "POST"])
 @login_required
-@exempt  # 🔥 This disables CSRF for this route
+  # 🔥 This disables CSRF for this route
 def profile():
     if current_user.role != "landlord":
         flash("Access denied.", "danger")
