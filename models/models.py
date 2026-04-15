e18cafcd5ebaaf3dc6c5a46116fb2c0ca8eb8293
kennedykirui766-beep <kennykirui766@gmail.com>
2026-04-16 00:58:18 +0300

    -

--- models/models.py
+++ models/models.py
@@ -271,6 +271,7 @@ class Message(db.Model):
     receiver_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     content = db.Column(db.Text)
     timestamp = db.Column(db.DateTime, default=db.func.current_timestamp())
+    is_read = db.Column(db.Boolean, default=False)
 
 
 class ServiceProvider(db.Model):
